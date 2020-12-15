62721973e5b57c78296d1c3f5f7484ce8bc372b1
sh-navid <shamshirsaz.navid@live.com>
2020-12-15 15:55:25 +0330

    nodejs file - package update

---
+++ nodejs/lesson04/script-file.js
@@ -0,0 +1,47 @@
+//Node 14.15.1 LTS
+
+const exp = require("express");
+const app = exp();
+const fs = require("fs");
+
+app.get("/html", (q, r) => {
+    r.send(`<h1>HTML</h1>`);
+});
+
+app.get("/file/sync", (q, r) => {
+    let data = fs.readFileSync("nodejs/lesson04/index.html");
+
+    data += "";
+    data = data.split("{{data}}").join("SYNC");
+
+    r.writeHead(200, { "Content-Type": "text/html" });
+    r.write(data);
+    r.end();
+});
+
+app.get("/file/async", (q, r) => {
+    fs.readFile("nodejs/lesson04/index.html", (err, data) => {
+        data += "";
+        data = data.split("{{data}}").join("A-SYNC");
+
+        r.writeHead(200, { "Content-Type": "text/html" });
+        r.write(data);
+        r.end();
+    });
+});
+
+let html = "";
+app.get("/file/:title", (q, r) => {
+    if (html === "") {
+        html = fs.readFileSync("nodejs/lesson04/index.html");
+        console.log("File Loaded");
+    }
+
+    data = (html+"").split("{{data}}").join(q.params.title);
+
+    r.writeHead(200, { "Content-Type": "text/html" });
+    r.write(data);
+    r.end();
+});
+
+app.listen(3000);
