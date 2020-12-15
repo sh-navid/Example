0c8add638a0520ba3a833e5b0d4951e1296ca9ad
sh-navid <shamshirsaz.navid@live.com>
2020-12-15 15:34:23 +0330

    nodejs params

---
+++ nodejs/lesson03/script-params.js
@@ -0,0 +1,15 @@
+//Node 14.15.1 LTS
+
+//npm install -g nodemon
+const exp = require("express");
+const app = exp();
+
+app.get("/api/v01/calc/:x", (q, r) => {
+    r.send("result(v01): " + q.params.x);
+});
+
+app.get("/api/v02/calc/:x/:y", (q, r) => {
+    r.send("result(v02): " + (q.params.x * q.params.y));
+});
+
+app.listen(3000);
