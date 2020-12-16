da09534cbb8a59edae4175a79561aef5a114a5a3
sh-navid <shamshirsaz.navid@live.com>
2020-12-16 14:30:21 +0330

    QuickSort

--- concepts/sorting-algorithms.js
+++ concepts/sorting-algorithms.js
@@ -7,6 +7,28 @@
 //---Avg
 //---Worst
 
-let QuickSort=()=>{
-    
+let QuickSort = (arr, low, high) => {
+    let ptr = { array: arr };
+    if (low < high) {
+        p = Partition(ptr, low, high);
+        QuickSort(ptr, low, p - 1);
+        QuickSort(ptr, p + 1, high);
+    }
+    return ptr.array;
 }
+
+let Partition = (ptr, low, high) => {
+    let pivot = ptr.array[high];
+    let i = low;
+    for (let j = low; j <= high; j++) {
+        if (ptr.array[j] < pivot) {
+            [ptr.array[i], ptr.array[j]] = [ptr.array[j], ptr.array[i]];
+            i++;
+        }
+    }
+    [ptr.array[i], ptr.array[high]] = [ptr.array[high], ptr.array[i]];
+    return i;
+}
+
+let arr, res = QuickSort(arr = [33, 22, 88, 23, 45, 0, 44, 11], 0, arr.length - 1);
+console.log(res);
