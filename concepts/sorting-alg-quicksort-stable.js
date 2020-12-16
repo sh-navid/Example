0647b5ffa0417a94edcbce43c62de67e55721f02
sh-navid <shamshirsaz.navid@live.com>
2020-12-16 16:11:24 +0330

    QuickSort - Stable, Not official version

--- concepts/sorting-alg-quicksort-stable.js
+++ concepts/sorting-alg-quicksort-stable.js
@@ -1,7 +1,7 @@
 //https://en.wikipedia.org/wiki/Sorting_algorithm#:~:text=Popular%20sorting%20algorithms%201%20Simple%20sorts.%20Two%20of,adding%20citations%20to%20reliable%20sources.%20More%20items...%20
 
 //Concepts:
-//---Stability      --This is Original and Unstable
+//---Stability      --Stable Version Not Original and is not Standard
 //---Memory
 //---Best
 //---Avg
@@ -13,23 +13,34 @@ QuickSort = (arr, low, high) => {
         QuickSort(arr, low, p - 1);
         QuickSort(arr, p + 1, high);
     }
-    return arr.A;
+    return arr;
 }
 
 let Partition = (arr, low, high) => {
-    console.log("Partition This: ", arr.A);
+    //console.log("Partition This: ", arr.A);
     let pivot = arr.A[high];
+    let index = arr.I[high];
     let i = low;
     for (let j = low; j <= high; j++) {
-        if (arr.A[j] < pivot) {
+        if (arr.A[j] + (arr.I[j] * influencer) < pivot + (index * influencer)) {
             [arr.A[i], arr.A[j]] = [arr.A[j], arr.A[i]];
+            [arr.I[i], arr.I[j]] = [arr.I[j], arr.I[i]];
             i++;
         }
     }
     [arr.A[i], arr.A[high]] = [arr.A[high], arr.A[i]];
+    [arr.I[i], arr.I[high]] = [arr.I[high], arr.I[i]];
     return i;
 }
 
-let arr = { A/* POINTER */: [33, 22, 88, 23, 45, 0, 44, 11] };
+let influencer = 0.0000001;
+
+let arr = {
+    I/* INDEXES */: [10, 11, 12, 13, 14, 15, 16, 17, 18, 19],
+    A/* POINTER */: [33, 22, 88, 33, 23, 45, 33, 89, 44, 11]
+};
 let res = QuickSort(arr, 0, arr.A.length - 1);
 console.log(res);
+
+console.log(34*influencer);
+console.log(35*influencer);
