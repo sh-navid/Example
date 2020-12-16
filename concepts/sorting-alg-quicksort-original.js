1b315026296c8631ad9a62af08a1d77f19b069e7
sh-navid <shamshirsaz.navid@live.com>
2020-12-16 14:51:33 +0330

    QuickSort

--- concepts/sorting-alg-quicksort-original.js
+++ concepts/sorting-alg-quicksort-original.js
@@ -1,7 +1,7 @@
 //https://en.wikipedia.org/wiki/Sorting_algorithm#:~:text=Popular%20sorting%20algorithms%201%20Simple%20sorts.%20Two%20of,adding%20citations%20to%20reliable%20sources.%20More%20items...%20
 
 //Concepts:
-//---Stability
+//---Stability      --This is Original and Unstable
 //---Memory
 //---Best
 //---Avg
