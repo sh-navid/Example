54f20b75fcfb3485afac04faef69e2c476bfd051
sh-navid <shamshirsaz.navid@live.com>
2020-12-16 23:06:32 +0330

    load file using fs and __dirname

--- nodejs/lesson04/script-file-dirname.js
+++ nodejs/lesson04/script-file-dirname.js
@@ -3,14 +3,14 @@
 const exp = require("express");
 const app = exp();
 const fs = require("fs");
-const pth=require("path");
+const pth = require("path");
 
 app.get("/html", (q, r) => {
     r.send(`<h1>HTML</h1>`);
 });
 
 app.get("/file/sync", (q, r) => {
-    let data = fs.readFileSync(pth.join(__dirname,"index.html"));
+    let data = fs.readFileSync(pth.join(__dirname, "index.html"));
 
     data += "";
     data = data.split("{{data}}").join("SYNC");
@@ -21,7 +21,7 @@ app.get("/file/sync", (q, r) => {
 });
 
 app.get("/file/async", (q, r) => {
-    fs.readFile(pth.join(__dirname,"index.html"), (err, data) => {
+    fs.readFile(pth.join(__dirname, "index.html"), (err, data) => {
         data += "";
         data = data.split("{{data}}").join("A-SYNC");
 
@@ -34,11 +34,11 @@ app.get("/file/async", (q, r) => {
 let html = "";
 app.get("/file/:title", (q, r) => {
     if (html === "") {
-        html = fs.readFileSync(pth.join(__dirname,"index.html"));
+        html = fs.readFileSync(pth.join(__dirname, "index.html"));
         console.log("File Loaded");
     }
 
-    data = (html+"").split("{{data}}").join(q.params.title);
+    data = (html + "").split("{{data}}").join(q.params.title);
 
     r.writeHead(200, { "Content-Type": "text/html" });
     r.write(data);
