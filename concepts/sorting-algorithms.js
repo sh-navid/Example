3a2efd29b5030340ed9ff54171cc07e8a2b9864c
sh-navid <shamshirsaz.navid@live.com>
2020-12-16 14:15:51 +0330

    QuickSort

---
+++ concepts/sorting-algorithms.js
@@ -0,0 +1,12 @@
+//https://en.wikipedia.org/wiki/Sorting_algorithm#:~:text=Popular%20sorting%20algorithms%201%20Simple%20sorts.%20Two%20of,adding%20citations%20to%20reliable%20sources.%20More%20items...%20
+
+//Concepts:
+//---Stability
+//---Memory
+//---Best
+//---Avg
+//---Worst
+
+let QuickSort=()=>{
+    
+}
