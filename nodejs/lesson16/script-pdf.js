b167aaab92272441fc80ee03e79f4c99d0521453
sh-navid <shamshirsaz.navid@live.com>
2020-12-17 22:31:35 +0330

    PDF

--- nodejs/lesson16/script-pdf.js
+++ nodejs/lesson16/script-pdf.js
@@ -3,16 +3,37 @@
 
 //npm install phantom
 //yarn add phantom
+//npm install ip
+//yarn add ip
 
-const phantom = require('phantom');   
+const exp = require('express');
+const app = exp();
+const pth = require("path");
+const phantom = require('phantom');
+const ip = require("ip");
 
-phantom.create().then((ph)=> {
-    ph.createPage().then((page)=> {
-        page.open("http://www.google.com").then((status)=> {
-            page.render('nodejs/data/pdf/output.pdf').then(()=> {
-                console.log('Page Rendered');
-                ph.exit();
-            });
+const PORT = 3000;
+const PDF_SOURCE = "index";//index.html
+const PDF_OUTPUT = "output";//output.pdf
+
+app.use("/" + PDF_SOURCE, exp.static(pth.join(__dirname, "", PDF_SOURCE + ".html")));
+app.use("/" + PDF_OUTPUT, exp.static(pth.join(__dirname, "", PDF_OUTPUT + ".pdf")));
+
+app.listen(PORT);
+
+let makePDF = async (fn) => {
+    let local = `http://${ip.address()}:${PORT}/${PDF_SOURCE}`;
+    phantom.create().then((ph) => {
+        ph.createPage().then((page) => {
+            page.open(local).then(() =>
+                page.render(`${PDF_OUTPUT}.pdf`).then(() => { ph.exit(); fn() })
+            );
         });
     });
+}
+
+makePDF(() => {
+    console.log("PDF Created From Local File");
+    console.log("PDF is downloadable from link:");
+    console.log(`http://${ip.address()}:${PORT}/${PDF_OUTPUT}`);
 });
