69cc34c48c1108c56de60c89314d52677d05df63
sh-navid <shamshirsaz.navid@live.com>
2020-12-26 13:07:54 +0330

    Validator Base64

--- nodejs/lesson16-validation/script-validator.js
+++ nodejs/lesson16-validation/script-validator.js
@@ -2,12 +2,22 @@
 
 const val = require('validator');
 
-console.log(val.isEmail('foo@bar.com'),"Email");
+console.log(val.isEmail('aa@bb.com'),"This is an email");
 
-console.log(val.equals('AA','AA'),"String Compare");
+console.log(val.equals('AA','AA'),"First string is equal to second string");
 
-console.log(val.isAfter('2012-02-11','2012-02-10'),"Date Compare");
+console.log(val.isAfter('2012-02-11','2012-02-10'),"First Date is after second date");
 
-console.log(val.isEmail('foo@bar.com'));
+console.log(val.isAlpha("سس","fa-IR"),"Is Alphabets");
 
-console.log(val.isEmail('foo@bar.com'));
+console.log(val.isAlphanumeric("سس","fa-IR"),"Is Alphanumeric");
+
+console.log(val.isAscii("AaBb"),"Is Ascii");
+
+console.log(val.isBase64("aGVsbG8="),"Is isBase64");
+
+console.log(val.isBase64("aGVsbG8="),"Is isBase64");
+console.log(val.isBase64("aGVsbG8="),"Is isBase64");
+console.log(val.isBase64("aGVsbG8="),"Is isBase64");
+console.log(val.isBase64("aGVsbG8="),"Is isBase64");
+console.log(val.isBase64("aGVsbG8="),"Is isBase64");
