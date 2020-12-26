c7f970675ba55745167914e558675706489993d0
sh-navid <shamshirsaz.navid@live.com>
2020-12-26 13:10:05 +0330

    validator Banking

--- nodejs/lesson16-validation/script-validator.js
+++ nodejs/lesson16-validation/script-validator.js
@@ -2,11 +2,18 @@
 
 const val = require('validator');
 
+//banking
+//--swift   -> isBIC()
+//--BTC     -> isBtcAddress()
+//--Credit  -> isCreditCard()
+
 console.log(val.isEmail('aa@bb.com'),"This is an email");
 
 console.log(val.equals('AA','AA'),"First string is equal to second string");
 
 console.log(val.isAfter('2012-02-11','2012-02-10'),"First Date is after second date");
+
+console.log(val.isBefore('2012-02-11','2012-02-10'),"First Date is before second date");
 
 console.log(val.isAlpha("سس","fa-IR"),"Is Alphabets");
 
