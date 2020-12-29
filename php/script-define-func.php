5cc5b3901e06c8187352f5e0d7567634035d4c51
sh-navid <shamshirsaz.navid@live.com>
2020-12-30 01:11:00 +0330

    php

--- php/script-define-func.php
+++ php/script-define-func.php
@@ -1,18 +1,27 @@
 <?php
 
-$x=12;
-$y=140;
+$x = 12;
+$y = 140;
+$z = 600;
 
-function fn_print(){
+
+
+function fn_print()
+{
+    static $q = 90;//static in memory
     global $x;
-    $y=14;
+    $y = 14;
 
     $x++;
     $y++;
+    $GLOBALS["z"]++;
+    $q++;
 
-    echo "IN FUNCTION ".$x." ".$y."\n";
+    echo "IN FUNCTION " . $x . " " . $y . " " . $GLOBALS["z"] . " " . $q . "\n";
 }
 
 fn_print();
+fn_print();
+fn_print();
 
-echo "OUT OF FUNCTION ".$x." ".$y;
+echo "OUT OF FUNCTION " . $x . " " . $y . " " . $z . "\n";
