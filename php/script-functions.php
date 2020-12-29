995e28223178d0690c94bef84efe88f178643be3
sh-navid <shamshirsaz.navid@live.com>
2020-12-30 00:57:43 +0330

    php

--- php/script-functions.php
+++ php/script-functions.php
@@ -1,6 +1,8 @@
 <?php
 $secret = "121212DDD";
 
+echo $secret;
+
 echo base64_encode($secret)."\n";
 
 echo base64_decode(base64_encode($secret))."\n";
@@ -10,3 +12,9 @@ echo strrev($secret)."\n";
 echo bin2hex($secret)."\n";
 
 echo strlen($secret)."\n";
+
+echo str_word_count($secret)."\n";
+
+echo chop($secret)."\n";//trim - strip
+
+echo str_replace("hello", "bye", "Hello world!");
