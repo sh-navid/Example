00a609833d12e1953fb330674613268964da5281
sh-navid <shamshirsaz.navid@live.com>
2020-12-30 00:41:55 +0330

    php functions

--- php/script-functions.php
+++ php/script-functions.php
@@ -3,6 +3,10 @@ $secret = "121212DDD";
 
 echo base64_encode($secret)."\n";
 
+echo base64_decode(base64_encode($secret))."\n";
+
 echo strrev($secret)."\n";
 
-echo bin2hex($secret);
+echo bin2hex($secret)."\n";
+
+echo strlen($secret)."\n";
