35d5d43386fc289d6fd6a778059335da586acd2b
sh-navid <shamshirsaz.navid@live.com>
2020-12-31 00:04:44 +0330

    Global   Variables

--- php/script-constant.php
+++ php/script-constant.php
@@ -1,9 +1,31 @@
 <?php
-define("DEF","Defined Parameter");
-define("ARR",["Item A","Item B"]);
+define("DEF", "Defined Parameter");
+define("ARR", ["Item A", "Item B"]);
 
-echo DEF."\n";
+echo DEF . "\n";
 
-echo ARR."\n";
+echo ARR . "\n";
 
-echo ARR[1]."\n";
+echo ARR[1] . "\n";
+
+$x = 12;
+
+//PHP Global Variables
+
+//$GLOBALS
+echo $GLOBALS["x"] . "\n\n";
+
+//$_SERVER
+foreach ($_SERVER as $x => $x_value) {
+    echo "Key=" . $x . ", Value=" . $x_value;
+    echo "\n";
+}
+echo "\n\n";
+
+//$_REQUEST
+//$_GET
+//$_POST
+//$_FILES
+//$_ENV
+//$_SESSION
+//$_COOKIE
