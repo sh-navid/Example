073c8b849922145531dd1c2f763234a8f26afe16
sh-navid <shamshirsaz.navid@live.com>
2020-12-31 14:29:26 +0330

    Stream

--- nodejs/lesson02-file-and-stream/script-stream-chain.js
+++ nodejs/lesson02-file-and-stream/script-stream-chain.js
@@ -3,7 +3,7 @@ const pth = require("path");
 
 var readerStream = fs.createReadStream(pth.join(__dirname, "index.html"));
 var readerStream2 = fs.createReadStream(pth.join(__dirname, "index.html"));
-var writerStream = fs.createWriteStream(pth.join(__dirname, "index_chained.html"));
+var writerStream = fs.createWriteStream(pth.join(__dirname, "chained.html"));
 
 //only readable streams have "pipe" method
 
