5896c2f0149702d282bfc0e37c5f812926677da2
sh-navid <shamshirsaz.navid@live.com>
2020-12-31 16:19:08 +0330

    stream and express

--- nodejs/lesson02-file-and-stream/script-stream-chain.js
+++ nodejs/lesson02-file-and-stream/script-stream-chain.js
@@ -1,10 +1,11 @@
+"use strict";
+
 const fs = require("fs");
 const pth = require("path");
 
 let readerStream = fs.createReadStream(pth.join(__dirname, "index.html"));
 let readerStream2 = fs.createReadStream(pth.join(__dirname, "index.html"));
 let writerStream = fs.createWriteStream(pth.join(__dirname, "chain1.html"));
-let writerStream2 = fs.createWriteStream(pth.join(__dirname, "chain2.html"));
 
 //only readable streams have "pipe" method
 
--- nodejs/lesson02-file-and-stream/script-stream-multi-chain.js
+++ nodejs/lesson02-file-and-stream/script-stream-multi-chain.js
@@ -6,13 +6,12 @@ const multi = require('multistream');
 const fs = require('fs');
 const pth = require("path");
 
-let writerStream = fs.createWriteStream(pth.join(__dirname, "multi-chain.html"));
- 
-var streams = [
+let streams = [
   fs.createReadStream(pth.join(__dirname, "index.html")),
   fs.createReadStream(pth.join(__dirname, "index.html")),
   fs.createReadStream(pth.join(__dirname, "index.html"))
-]
+];
+let writerStream = fs.createWriteStream(pth.join(__dirname, "multi-chain.html"));
  
-//new multi(streams).pipe(process.stdout);
+new multi(streams).pipe(process.stdout);
 new multi(streams).pipe(writerStream);
