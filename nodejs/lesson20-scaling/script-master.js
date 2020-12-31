3c2bfd4ced32233276096fed576bb34ba54ab884
sh-navid <shamshirsaz.navid@live.com>
2020-12-31 18:39:02 +0330

    fork, spawn, exec

--- nodejs/lesson20-scaling/script-master.js
+++ nodejs/lesson20-scaling/script-master.js
@@ -2,6 +2,10 @@ const fs = require('fs');
 const ch = require('child_process');
 const pth = require("path");
 
+//exec  -> run a command in console
+//spawn -> run a new process
+//fork  -> run a node process
+
 for (var i = 0; i < 5; i++) {
     var workerProcess = ch.fork(pth.join(__dirname, "script-child.js"), [i]);
 
