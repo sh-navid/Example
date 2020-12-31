696a7a2cacc5a785a98335c5dff222d45d584bf9
sh-navid <shamshirsaz.navid@live.com>
2020-12-31 10:48:28 +0330

    File and Stream

---
+++ nodejs/lesson02-file-and-stream/script-stream.js
@@ -0,0 +1,24 @@
+"use strict";
+
+const fs = require("fs");
+const pth = require("path");
+
+let data = '';
+
+const readerStream = fs.createReadStream(pth.join(__dirname, "index.html"));
+readerStream.setEncoding('UTF8');
+
+// Handle stream events --> data, end, and error
+readerStream.on('data', (chunk) => {
+    data += chunk;
+});
+
+readerStream.on('end', () => {
+    console.log(data);
+});
+
+readerStream.on('error', (err) => {
+    console.log(err.stack);
+});
+
+console.log("End of Node Program");
