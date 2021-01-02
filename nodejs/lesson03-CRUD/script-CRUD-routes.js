560e30656ba718ed9ac94c3b1e234236f0b279dc
sh-navid <shamshirsaz.navid@live.com>
2021-01-02 09:32:39 +0330

    CRUD

--- nodejs/lesson03-CRUD/script-CRUD-routes.js
+++ nodejs/lesson03-CRUD/script-CRUD-routes.js
@@ -5,17 +5,17 @@
 
 const exp = require('express');
 const app = exp();
-const router = require('express').Router();
+const rtr = require('express').Router();
 
 const PORT = 3000;
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
 
-router.route("/user")
+rtr.route("/user")
     .get((q, r, n) => { r.send("GET USER"); n(); })
     .post((q, r, n) => { r.send("POST USER "); n(); });
 
-router.route("/user/:id")
+rtr.route("/user/:id")
     .get((q, r, n) => { r.send("GET USER " + q.params.id); n(); })
     .put((q, r, n) => { r.send("PUT USER " + q.params.id); n(); })
     .delete((q, r, n) => { r.send("DELETE USER " + q.params.id); n(); });
